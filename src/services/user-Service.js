953aeb0d3bde0d5b68bd8cdf4075decdfef41747
kumar Abhinav <krabhinav023@gmail.com>
2024-07-25 20:32:21 +0530

    added signin with jwt creation

--- src/services/user-Service.js
+++ src/services/user-Service.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const { UserRepository,  } = require('../repositories');
+const { Auth } = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 
 const userRepo = new UserRepository();
@@ -23,9 +24,30 @@ async function create(data) {
     }
 }
 
+async function signin(data) {
+    try {
+        const user = await userRepo.getUserByEmail(data.email);
+        if(!user) {
+            throw new AppError('No user found for the given email', StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch = Auth.checkPassword(data.password, user.password);
+        console.log("password match", passwordMatch)
+        if(!passwordMatch) {
+            throw new AppError('Invalid password', StatusCodes.BAD_REQUEST);
+        }
+        const jwt = Auth.createToken({id: user.id, email: user.email});
+        return jwt;
+    } catch(error) {
+        if(error instanceof AppError) throw error;
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 
 module.exports = {
     create,
+    signin
 
    
 }
