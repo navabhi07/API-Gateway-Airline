8e51a80aea3aa207c1569864a0b51c7b662f3077
kumar Abhinav <krabhinav023@gmail.com>
2024-07-29 23:46:35 +0530

    added isauthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const { InfoController } = require('../../controllers');
 const userRouter = require('./user-routes');
+const { AuthRequestMiddlewares } = require('../../middlewares');
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+router.get('/info',AuthRequestMiddlewares.checkAuth ,InfoController.info);
 router.use('/user', userRouter);
 
 module.exports = router;
